3b2801b0735f3c9ad4d97fec082b64f50048d639
KevinMosweu <kevinloaded@hotmail.com>
2023-01-17 03:21:20 -0500

    Added comments to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,32 @@
+#Import necessary modules
 import os
 import csv
 
+#List to store dates
 date_list=[]
 
+#List to store monthly changes in profits/losses
 changes_list=[]
 
+#Set initial value for previous month proft/loss
 lastmonth = 0
 
+#Set initial value for total
 total = 0
 
+#Specify path for CSV file to be read
 budget_data_csv = os.path.join('Resources', 'budget_data.csv')
 
+#Open and read the CSV
 with open(budget_data_csv, encoding='utf') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
+    
+    #Store header
     csv_header = next(csvfile)
-
+    
+    #Loop through rows collect date,
+    #Add monthly profit/loss to total,
+    #Calculate change in profit/loss from previous month and store it
     for row in csvreader:
         date_list.append(row[0])
         total = total + int(row[1])
@@ -23,11 +34,16 @@ with open(budget_data_csv, encoding='utf') as csvfile:
         lastmonth = int(row[1])
         changes_list.append(change)
     
+    #Find greatest increase/decrease in monthly changes, find corresponding date in date list
     greatest_increase = [date_list[changes_list.index(max(changes_list))], max(changes_list)]
     greatest_decrease = [date_list[changes_list.index(min(changes_list))], min(changes_list)]
+    
+    #Remove first value of changes as technicaly there is no change for the first month
+    #Proceed to calculate average, rounded to two decimal places
     changes_list.pop(0)
     average_change = round(sum(changes_list)/len(changes_list), 2)
-
+    
+    #Print heading and calculates values to terminal
     print("Financial Analysis")
     print('')
     print('-------------------------------')
@@ -42,9 +58,10 @@ with open(budget_data_csv, encoding='utf') as csvfile:
     print('')
     print(f'Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})')
 
-
+#Specify path to write text file to
 fin_analysis_txt= os.path.join('Analysis', 'Financial_Analysis.txt')
 
+#Write heading and calculated values to text file
 with open(fin_analysis_txt, 'w') as textfile:
     textfile.write('Financial Analysis\n')
     textfile.write('\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,35 @@
+#Import necessary modules
 import os
 import csv
 
+#List to store each vote
 votes_list=[]
 
+#List to store each unique candidate
 candidate_list = []
 
+#Blank value to compare candidate names against
 candidate = ''
 
+#Set path for file to be read
 election_data_csv = os.path.join('Resources', 'election_data.csv')
 
+#Opening the CSV
 with open(election_data_csv, encoding='utf') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
+    
+    #Store header row
     csv_header = next(csvfile)
-
+    
+    #Loop through rows and store each vote as candidate name in vote_list
+    #Also compare if candidate name has been encountered and store in candidate_list if new
     for row in csvreader:
         votes_list.append(row[2])
         if row[2] != candidate and candidate_list.count(row[2])==0:
             candidate_list.append(row[2])
             candidate = row[2]
     
-    
+#Printing heading to terminal and also total votes by finding number of votes stored in vote_list    
 print("Election Results")
 print('')
 print('-------------------------')
@@ -29,6 +38,9 @@ print(f'Total Votes: {len(votes_list)}')
 print('')
 print('-------------------------')
 
+#Loop through list of unique candidates, 
+#Count the number of votes, calculate percentage to three decimal places
+#Print results to terminal
 for candidate in candidate_list:
     votes = votes_list.count(candidate)
     percentage = round((votes/len(votes_list))*100, 3)
@@ -39,13 +51,16 @@ for candidate in candidate_list:
 print('-------------------------')
 print('')
 
+#Find winner by finding most frequent name appearing in votes list
 mode = max(set(votes_list), key = votes_list.count)
 
-
+#Print winner to terminal
 print(f'Winner: {mode}')
 
+#Set path to write text file to
 elec_results_txt= os.path.join('Analysis', 'Election_Analysis.txt')
 
+#Writing previously found results to text file in path above
 with open(elec_results_txt, 'w') as textfile:
     textfile.write('Election Results\n')
     textfile.write('\n')
